abbb25e9f282d0d6a61a04755d419947ac2fb41b
Taniel49 <laughingstorm49@gmail.com>
2023-02-16 02:11:18 +0300

    textarea added

--- src/components/CharacterResult/CharacterResult.js
+++ src/components/CharacterResult/CharacterResult.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-function CharacterResult({name, result, index, removeCharacter}){
+function CharacterResult({name, result, index, removeCharacter}) {
+    const [isTextareaOpened, setTextareaOpened] = React.useState('false');
+
     return (
-        <li>{name}:{result}<button type={'button'} onClick={()=>{removeCharacter(index)}}> remove</button></li>
+        <li>
+            <button type={'button'}
+            onClick={() => {
+                setTextareaOpened(!isTextareaOpened);
+            }}>
+                <p>{name}:{result}</p>
+            </button>
+            <textarea style={{display: isTextareaOpened ? 'block' : 'none'}}/>
+            <button type={'button'} onClick={() => {
+                removeCharacter(index)
+            }}> remove
+            </button>
+        </li>
     );
 }
 
